4afb948805c79c4e92d221149e4a3ca442ba4b56
fbsmyuming <56677126+fbsmyuming@users.noreply.github.com>
2019-10-18 22:38:08 +0800

    Add files via upload

---
+++ scrpits/get_my_date.py
@@ -0,0 +1,8 @@
+from mydates import Dates
+from mydates import print_test
+birth_day = '1990-09-13'
+wedding_day = '2018-06-26'
+mydays = Dates.Important_Dates(birth_day,wedding_day)
+mydays.get_days_passed_in_my_life()
+mydays.get_days_passed_in_marriage()
+print_test()
